ec559096fbd366bea20b2e545b4093e9707c49cd
GonzoTrickster9899 <lancelotserrano.oleraina@bicol-u.edu.ph>
2025-07-13 13:12:21 +0800

    Stable 2.0.6

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import Experience from "./Components/Experience/Experience";
 import Works from "./Components/Works/Works";
 import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonial from "./Components/Testimonial/Testimonial";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Experience />
       <Works />
       <Portfolio />
+      <Testimonial />
+      <Contact />
     </div>
   );
 }
